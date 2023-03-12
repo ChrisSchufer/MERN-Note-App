f96da1dd481bceeb6105ac4a05b8417d6986212f
Christoph Schufer <74572716+deiVodda@users.noreply.github.com>
2023-03-12 02:35:09 +0100

    with the /api/users api I split the code into seperate files

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -1,15 +1,5 @@
 import { Note } from "../models/notes";
-
-async function fetchData(input: RequestInfo, init?: RequestInit) {
-  const response = await fetch(input, init);
-  if (response.ok) {
-    return response;
-  } else {
-    const errorBody = await response.json();
-    const errorMessage = errorBody.error;
-    throw Error(errorMessage);
-  }
-}
+import fetchData from "./fetchData";
 
 export async function fetchNotes(): Promise<Note[]> {
   const response = await fetchData("/api/notes", {
